4515404a314c2cb730ee010c4335da2bad6391fa
Carlos <carlosicaraballo1999@gmail.com>
2025-02-13 14:33:40 +0000

    182. Añadiendo el componente RESTFul Controller

--- src/main/java/com/carlos/curso/springboot/app/springbootcrud/controllers/ProductController.java
+++ src/main/java/com/carlos/curso/springboot/app/springbootcrud/controllers/ProductController.java
@@ -5,6 +5,7 @@ import com.carlos.curso.springboot.app.springbootcrud.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,5 +61,19 @@ public class ProductController {
   ) {
     Product newProduct = this.productService.save(product);
     return ResponseEntity.status(HttpStatus.CREATED).body(newProduct);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> delete(
+    @PathVariable Long id
+  ) {
+    Product product = new Product();
+    product.setId(id);
+    Optional<Product> productOptional = this.productService.remove(product);
+    if (productOptional.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    return ResponseEntity.status(HttpStatus.OK).body(productOptional.orElseThrow());
   }
 }
