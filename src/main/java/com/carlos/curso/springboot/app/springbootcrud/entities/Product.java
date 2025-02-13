7adaca59d41e35be54b4429a11ebcbf404dd50be
Carlos <carlosicaraballo1999@gmail.com>
2025-02-13 18:59:27 +0000

    187. Mensajes de errores personalizados usando properties

--- src/main/java/com/carlos/curso/springboot/app/springbootcrud/entities/Product.java
+++ src/main/java/com/carlos/curso/springboot/app/springbootcrud/entities/Product.java
@@ -8,8 +8,6 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
@@ -23,7 +21,7 @@ public class Product {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @NotEmpty
+  @NotEmpty(message = "{NotEmpty.product.name}")
   @Size(min=3, max=20)
   private String name;
 
