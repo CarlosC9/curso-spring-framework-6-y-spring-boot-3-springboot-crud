131f1d04e44f574fa9df562d344572d9ebe5f903
Carlos <carlosicaraballo1999@gmail.com>
2025-02-13 14:04:32 +0000

    180. Creando clases entity y repository

---
+++ src/main/java/com/carlos/curso/springboot/app/springbootcrud/entities/Product.java
@@ -0,0 +1,87 @@
+package com.carlos.curso.springboot.app.springbootcrud.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private Integer price;
+
+  private String description;
+
+  public Product() {
+  }
+
+  public Product(String name, Integer price, String description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getPrice() {
+    return price;
+  }
+
+  public void setPrice(Integer price) {
+    this.price = price;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public String toString() {
+    return "Product{" +
+      "id=" + id +
+      ", name='" + name + '\'' +
+      ", price=" + price +
+      ", description='" + description + '\'' +
+      '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    Product product = (Product) o;
+    return Objects.equals(id, product.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
+  }
+}
---
+++ src/main/java/com/carlos/curso/springboot/app/springbootcrud/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.carlos.curso.springboot.app.springbootcrud.repositories;
+
+import com.carlos.curso.springboot.app.springbootcrud.entities.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
